e45841a54980c265764292851c0c262c956552fb
BrunaBelo <anabruna28@gmail.com>
2021-05-22 12:48:55 -0300

    adicionar autenticacao

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  # GET /users or /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: "User was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:name, :role_id, :email, :password)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+role1 = Role.create({ name: 'admin'})
+role2 = Role.create({ name: 'funcionario_marketing'})
+
+user1 = User.create({ name: 'Ana', email: 'ana@gmail.com', password: '123456', password_confirmation: '123456', role_id: role1.id })
+user1 = User.create({ name: 'Bruna', email: 'bruna@gmail.com', password: '123456', password_confirmation: '123456', role_id: role2.id })
