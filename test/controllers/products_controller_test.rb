162c1795a9099c275da4d8728f5a74eab99c3bb4
BrunaBelo <anabruna28@gmail.com>
2021-05-24 12:10:19 -0300

    adicionar testes

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,19 +1,30 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:employee_marketing)
+    @product = products(:one)
+  end
+
   test "should get index" do
-    get products_index_url
+    get product_listing_products_path
+    assert_response :success
+  end
+  
+  test "should get index admin" do
+    get products_path
     assert_response :success
   end
 
-  test "should get new" do
-    get products_new_url
+  test "should update product" do
+    patch product_url(@product), params: { product: { name: @product.name } }
     assert_response :success
   end
 
-  test "should get create" do
-    get products_create_url
+  test "should get new admin" do
+    get new_product_path
     assert_response :success
   end
-
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,48 +1,83 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-  end
+    include Devise::Test::IntegrationHelpers
 
-  test "should get index" do
-    get users_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_user_url
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: { name: @user.name, role_id: @user.role_id } }
+    setup do
+        sign_in users(:admin)
+        @user = users(:admin)
+        @user.add_role(:admin)
     end
 
-    assert_redirected_to user_url(User.last)
-  end
-
-  test "should show user" do
-    get user_url(@user)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_user_url(@user)
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch user_url(@user), params: { user: { name: @user.name, role_id: @user.role_id } }
-    assert_redirected_to user_url(@user)
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user)
+    test "should get index" do
+        get users_url
+        assert_response :success
     end
 
-    assert_redirected_to users_url
-  end
+    test "should not get index employee_marketing" do
+        sign_in users(:employee_marketing)
+        @user = users(:employee_marketing)
+        @user.add_role(:employee_marketing)
+        assert_raise CanCan::AccessDenied do
+            get users_url
+        end        
+    end
+
+    test "should get new" do
+        get new_user_path
+        assert_response :success
+    end
+
+    test "should not get new employee_marketing" do
+        sign_in users(:employee_marketing)
+        @user = users(:employee_marketing)
+        @user.add_role(:employee_marketing)
+        assert_raise CanCan::AccessDenied do
+            get new_user_path
+        end        
+    end
+
+    test "should get edit" do
+        get edit_user_url(@user)
+        assert_response :success
+    end
+
+    test "should not get edit employee_marketing" do
+        sign_in users(:employee_marketing)
+        @user = users(:employee_marketing)
+        @user.add_role(:employee_marketing)
+        assert_raise CanCan::AccessDenied do
+            get edit_user_url(@user)
+        end        
+    end
+
+    test "should update user" do
+        patch user_url(@user), params: { user: { name: @user.name, role_id: @user.roles } }
+        assert_redirected_to users_url
+    end
+
+    test "should not get update employee_marketing" do
+        sign_in users(:employee_marketing)
+        @user = users(:employee_marketing)
+        @user.add_role(:employee_marketing)
+        assert_raise CanCan::AccessDenied do
+            get patch user_url(@user), params: { user: { name: @user.name, role_id: @user.roles } }
+        end        
+    end
+
+    test "should destroy user" do
+        assert_difference('User.count', -1) do
+            delete user_url(@user)
+        end
+        assert_redirected_to users_url
+    end
+
+    test "should not get destroy employee_marketing" do
+        sign_in users(:employee_marketing)
+        @user = users(:employee_marketing)
+        @user.add_role(:employee_marketing)
+        assert_raise CanCan::AccessDenied do
+            delete user_url(@user)
+        end        
+    end
 end
